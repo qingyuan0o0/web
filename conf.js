fb3200fef7bf20ae26fcd44263c7958d9b22111a
情缘 <phil_god@139.com>
2020-04-24 16:57:36 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -7,30 +7,6 @@ jsproxy_config({
 
   // 节点配置
   node_map: {
-    'defalut': {
-      label: '别人的第1通道',
-      lines: {
-        'bitter-mouse-f2f5.mitian.workers.dev': 1,
-      }
-    },
-    'defalut1': {
-      label: '别人的第2通道',
-      lines: {
-        'purple-mountain-cc87.mitian.workers.dev': 1,
-      }
-    },
-    'defalut2': {
-      label: '别人的第3通道',
-      lines: {
-        'frosty-wave-56ec.mitian.workers.dev': 1,
-      }
-    },
-      'defalut3': {
-      label: '别人的第4通道',
-      lines: {
-        'fancy-hall-fec2.mitian.workers.dev': 1,
-      },
-    },
     'mysite': {
       label: '当前站点',
       lines: {
@@ -62,7 +38,6 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  assets_cdn: 'https://dop.imfast.io/assets/',
 
   // 本地测试时打开，否则访问的是线上的
   // assets_cdn: 'assets/',
