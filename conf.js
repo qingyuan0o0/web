01b437a5b6c090094ffe269bfd50bfd353108b88
情缘 <phil_god@139.com>
2020-04-26 09:05:54 +0800

    Add files via upload

--- conf.js
+++ conf.js
@@ -1,5 +1,10 @@
 jsproxy_config({
+  // 当前配置的版本（记录在日志中，用于排查问题）
+  // 每次修改配置，该值需要增加，否则不会生效。
+  // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
   ver: '118',
+
+  // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
     enable: true,
     ver: 79
@@ -7,6 +12,21 @@ jsproxy_config({
 
   // 节点配置
   node_map: {
+    'demo-hk': {
+      label: '演示服务-香港节点',
+      lines: {
+        // 主机:权重
+        'node-aliyun-hk-0.etherdream.com:8443': 1,
+        'node-aliyun-hk-1.etherdream.com:8443': 1,
+        'node-aliyun-hk-2.etherdream.com:8443': 1,
+      }
+    },
+    'demo-sg': {
+      label: '演示服务-新加坡节点',
+      lines: {
+        'node-aliyun-sg.etherdream.com:8443': 1,
+      },
+    },
     'mysite': {
       label: '当前站点',
       lines: {
@@ -38,13 +58,14 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+  //assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+  assets_cdn: 'https://cdn.jsdelivr.net/gh/qingyuan0o0/web@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
   // assets_cdn: 'assets/',
 
   // 首页路径
-  index_path: 'index.html',
+  index_path: 'index_v3.html',
 
   // 支持 CORS 的站点列表（实验中...）
   direct_host_list: 'cors_v1.txt',
@@ -61,8 +82,8 @@ jsproxy_config({
     'https://www.baidu.com/img/baidu_resultlogo@2.png': {
       replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
     },
-    'http://xn--7p0a.com/': {
-      content: 'Hello World!'
+    'http://haha.com/': {
+      content: 'Hello World'
     },
   }
 })
